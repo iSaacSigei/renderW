d4fa2db1a36a2fdd655766ace8258974e07cfd19
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-10 02:41:36 +1000

    Cors config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Rails.application.configure do
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
   # config.assume_ssl = true
+  config.hosts << "https://www.womall.africa/"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
