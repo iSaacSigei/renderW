2d45ce24c7d011dbc648e3dd26d3767441fb716a
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-10 03:05:00 +1000

    cors=3

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins 'https://wo-mall-git-master-isaacsigeis-projects.vercel.app/','https://www.womall.africa/','https://wo-mall-a8gd2hz7n-isaacsigeis-projects.vercel.app/'
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
