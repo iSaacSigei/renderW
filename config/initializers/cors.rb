6af7820ecdf6b60be3c7a0112be26bb46c91078b
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-10 03:15:20 +1000

    Render domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'https://wo-mall-git-master-isaacsigeis-projects.vercel.app/','https://www.womall.africa/','https://wo-mall-a8gd2hz7n-isaacsigeis-projects.vercel.app/'
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
+  allow do
+    origins 'https://www.womall.africa', 'https://mysite-jr5y.onrender.com' # Allow both domains
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
-  
+end
