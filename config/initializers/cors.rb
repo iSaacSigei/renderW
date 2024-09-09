6bd8fece0af7a5abc92199eb9c3ead2436c588cd
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-10 02:39:52 +1000

    Cors configurations

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://wo-mall.vercel.app'
+      origins 'https://www.womall.africa'
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
