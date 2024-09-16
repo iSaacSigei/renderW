02df78716fad24f17cf225ecd2e59f87538ae3d6
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-16 20:28:45 +1000

    cors syntax-correction

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.womall.africa', 'https://mysite-jr5y.onrender.com', 'https://www.eigoll.co.ke',# Allow both domains
+    origins 'https://www.womall.africa', 'https://mysite-jr5y.onrender.com', 'https://www.eigoll.co.ke'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
