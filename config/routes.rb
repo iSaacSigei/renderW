bd66feebf01ae36eaa687051a2b3aeb54baf4123
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-23 20:36:27 +1000

    tender controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  resources :tenders, only: [:index, :show, :create, :update, :destroy]
+  resources :tenders do
+    collection do
+      patch 'update_category'
+    end
+  end
   resources :jobs, only: [:index, :show, :create, :update, :destroy]
   post '/signup', to: 'users#create'
   post 'login', to: 'sessions#login'
-  get 'user', to: 'sessions#show'  # Add this line
+  get 'user', to: 'sessions#show'
   resources :users, only: [:create, :update, :destroy, :index]
   resources :export_orders, only: [:create, :index, :show]
   resources :import_orders, only: [:create, :index, :show]
   resources :quotations, only: [:index, :create, :show]
   resources :contact_messages, only: [:create, :index]
-  patch 'tenders/update_category', to: 'tenders#update_category'
-
-  
 end
