dbb88bac3cdd5daf2480b16a04945a96a23c3327
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-29 23:54:18 +1000

    Attachment

--- app/mailers/quotation_mailer.rb
+++ app/mailers/quotation_mailer.rb
@@ -4,6 +4,8 @@ class QuotationMailer < ApplicationMailer
     @order = @quotation.import_order || @quotation.export_order
     @user = @order.user  # Send email to the user who owns the import/export order
     @order_type = @quotation.import_order ? 'import order' : 'export order'
+   # Attach the image
+   attachments['WoMall.png'] = File.read(Rails.root.join('storage', 'WoMall.png'))
 
     mail(to: @user.email, subject: 'Your Quotation Has Been Processed')
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
     def welcome_email(user)
       @user = user
       @url  = 'https://womall.africa/login'
+      # Attach the image
+      attachments['WoMall.png'] = File.read(Rails.root.join('storage', 'WoMall.png'))
       mail(to: @user.email, subject: 'Welcome to WoMall!') do |format|
         format.html { render 'welcome_email' }
       end
