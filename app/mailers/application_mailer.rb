27b6a19007559fac5a496aa8c435037b68ff4f41
iSaacSigei <kiplangatisack.is@gmail.com>
2024-09-29 23:38:11 +1000

    womall name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'admin@womall.africa'
+  default from: 'admin@womall.africa', 
+  reply_to: 'admin@womall.africa', 
+  headers: { 'X-SMTPAPI' => { 'filters' => { 'templates' => { 'settings' => { 'from' => { 'name' => 'WoMall' } } } } } } }
+
   layout "mailer"
 end
